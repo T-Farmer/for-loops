7a30ddf13f1e8b04baf84c9aa55ca3b2ee45a827
T-Farmer <terrill@Rels-Pro-186.local>
2016-12-01 08:42:44 -0600

    presidents exercise

--- for-loops.js
+++ for-loops.js
@@ -1,6 +1,18 @@
 for(var i = 5; i <= 120; i+=10){
 	console.log(i);
 }
+
+
+
 for(var i = 4096; i >= 4096; i /= 2){
 	console.log(i);
 }
+
+
+
+var presidents = ["George Washington", "John Adams", "Thomas Jefferson"];
+var presidentsLength = presidents.length;
+	for(var i = 0; i < presidentsLength; i++){
+		console.log("President #" + (i + 1) + " was " + presidents[i]);
+	}
+	
